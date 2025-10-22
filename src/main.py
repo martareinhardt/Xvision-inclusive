95dda897d18753b4c915f31d51bdd4c295c5eda7
martareinhardt <marta.silveira15@hotmail.com>
2025-10-22 17:28:59 -0300

    Delete src/main.py

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from modules.survey import get_user_tier, collect_interests
-
-def main():
-    tier = get_user_tier()
-    interests = collect_interests(tier)
-    print(f"Interesses escolhidos: {interests}")
-
-if __name__ == "__main__":
-    main()
